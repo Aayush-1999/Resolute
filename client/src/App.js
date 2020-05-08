d6c2d2da46d11065e8ea61a75b3b460411594110
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-05-09 01:34:33 +0530

    Add auth and registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Route , Redirect, Switch} from 'react-router-dom';
+import { connect } from 'react-redux';
+import LoginEmail from './Containers/login/email/login-email';
+import LoginPassword from './Containers/login/password/login-password';
+import SignUp from './Containers/signup/signup';
+import Layout from './hoc/Layout/Layout';
+import Home from './Containers/Home/Home';
+import * as actions from './store/actions/index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  componentDidMount(){
+    this.props.onCheckAuthState()
+  }
+
+  render(){
+    let routes=(
+      <Switch>
+        <Route path="/" exact><Redirect to="/signin" /></Route>
+        <Route path="/signin" exact component={LoginEmail} />
+        <Route path="/signin/pwd" exact component={LoginPassword} />
+        <Route path="/signup" exact component={SignUp} />
+      </Switch>
+    )
+    
+    if(this.props.isAuthenticated){
+      routes=(
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      )
+    }
+    return(
+      <Layout>
+        {routes}
+      </Layout>
+    )
+  }
 }
 
-export default App;
+const mapStateToProps=state=>{
+  return{
+    isAuthenticated:state.auth.authenticated
+  }
+}
+
+const mapDispatchToProps=dispatch=>{
+  return{
+    onCheckAuthState:()=>dispatch(actions.authCheckState())
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
