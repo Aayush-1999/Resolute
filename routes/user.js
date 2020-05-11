b75ac7bde8c6f49c178633386cc1fc957f91b61e
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-05-11 13:15:31 +0530

    Add user route

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,22 @@ const express = require("express"),
     User      = require("../models/user"),
     Doctor    = require("../models/doctor");
 
-router.get("/",(req,res)=>{
+router.get("/",async (req,res)=>{
+    try{
+        let user = User.findById(req.body.userId);
+        console.log(user);
+        let doctors=[];
+        user.doctor.map((doctorId,index)=>{
+            let doctor =  Doctor.findById(doctorId)
+            console.log(doctor)
+            doctors.push(doctor)
+        })
+        res.status(200).json(doctors)
+    }
+    catch(err){
+        console.log(err)
+        res.status(400)
+    }
 
 })
 
